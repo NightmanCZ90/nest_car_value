eb34c5f9ed8b4581817c63ac50804e46477c554b
NightmanCZ90 <novak.jan90@gmail.com>
2022-08-24 01:36:27 +0200

    feat: Implement price estimation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, Patch, Param } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Patch, Param, Get, Query } from '@nestjs/common';
 import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
@@ -8,11 +8,16 @@ import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
-
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) { }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
   
   @Post()
   @UseGuards(AuthGuard)
